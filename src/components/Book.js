5852ad566474ab695bc0a37e09d6259084b7d4a6
Leslie Glaves <leslie@use-logic.com>
2017-09-02 12:11:29 -0400

    feat: Removed ShelfChanger and reverted to starter project book-shelf-changer.

--- src/components/Book.js
+++ src/components/Book.js
@@ -17,7 +17,15 @@ class Book extends Component {
                             height: 193,
                             backgroundImage: 'url(' + this.props.book.imageLinks.thumbnail + ')'
                         }}>
-                            <ShelfChanger moves={this.moveBookData} shelf={this.props.book.shelf}/>
+                        </div>
+                        <div className="book-shelf-changer">
+                            <select>
+                                <option value="none" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                            </select>
                         </div>
                     </div>
                     <div className="book-title">

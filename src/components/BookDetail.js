3ee93df7d59e2ea294cac64cf8b982a107a8f6c5
Leslie Glaves <leslie@use-logic.com>
2017-09-23 16:30:10 -0400

    feat: updated code for book detail page

--- src/components/BookDetail.js
+++ src/components/BookDetail.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
+import * as BooksAPI from '../BooksAPI'
 
 // BookDetail Component displays more details for a single book
 
 class BookDetail extends Component {
 
+    state = {
+        book: {},
+    }
+
+    componentWillMount() {
+        const { bookId } = this.state
+        this.book = this.getBook(bookId)
+
+    }
+
+    getBook() {
+        BooksAPI.get(this.props.bookId).then(book => {
+            this.setState({book})
+        })
+    }
 
     render() {
+        const { book } = this.state
 
-        const { book } = this.props
-
+        // TODO: make multiple authors display properly
         return (
             <div className="list-books">
                 <div className="list-books-title">
@@ -16,12 +32,19 @@ class BookDetail extends Component {
                     <h1>Book Detail</h1>
                 </div>
 
+                <div className="book-detail">
+                    <div className="book-cover-detail" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail : ''})`}}></div>
+                    <div className="book-title-detail">
+                        <h3>{book.title}</h3>
+                    </div>
+                    <div className="book-detail-authors">Written by {book.authors ? book.authors[0] : ''}</div>
+                    <div className="book-detail-description"> <h3>Description:</h3> {book.description} </div>
+                </div>
 
 
             </div>
         )
     }
 }
-
 
 export default BookDetail
